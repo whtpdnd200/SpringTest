850dc672e7ccd8860c6558efb974813e0a68107c
whtpdnd <whtpdnd200@gmail.com>
2025-11-17 18:47:31 +0900

    [조세웅] - 과제 진행중

--- src/main/java/com/devwork/springtest/mvc/RealtorController.java
+++ src/main/java/com/devwork/springtest/mvc/RealtorController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,7 +22,7 @@ public class RealtorController {
         return "mvc/realtorAdd";
     }
 
-    @PostMapping("/info")
+    @GetMapping("/info")
     public String responseInfo(
             Model model
             , @RequestParam("office") String office
