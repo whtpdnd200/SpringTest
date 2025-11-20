ab2a3d087b9392e604d4ef2f1afa514acc63d168
whtpdnd200 <whtpdnd200@gmail.com>
2025-11-20 20:25:00 +0900

    [조세웅] - 에이잭스 과제 완료

--- src/main/java/com/devwork/springtest/ajax/repository/BookMarkRepository.java
+++ src/main/java/com/devwork/springtest/ajax/repository/BookMarkRepository.java
@@ -1,10 +1,15 @@
 package com.devwork.springtest.ajax.repository;
 
+import com.devwork.springtest.ajax.domain.BookMark;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface BookMarkRepository {
 
     public int insertBookMark(@Param("title") String title, @Param("url") String url);
+
+    public List<BookMark> selectBookMarkList();
 }
